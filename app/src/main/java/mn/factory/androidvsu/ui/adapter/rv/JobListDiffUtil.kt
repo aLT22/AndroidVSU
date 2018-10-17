4802fbf1b581d39f0c7094a21b96651b90299c8f
Alexey Turkin <altsworks@gmail.com>
2018-10-17 12:24:20 +0300

    added callable diff util class for caclulating differences in job list

---
+++ app/src/main/java/mn/factory/androidvsu/ui/adapter/rv/JobListDiffUtil.kt
@@ -0,0 +1,62 @@
+package mn.factory.androidvsu.ui.adapter.rv
+
+import android.support.v7.util.DiffUtil
+import mn.factory.androidvsu.model.adzuna.JobPresentation
+import java.util.concurrent.Callable
+
+/**
+ * Created by Turkin A. on 17/10/2018.
+ */
+class JobListDiffUtil(
+        private val jobs: List<JobPresentation>,
+        private val items: List<JobPresentation>
+) : DiffUtil.Callback(), Callable<DiffUtil.DiffResult> {
+
+    override fun areItemsTheSame(p0: Int, p1: Int): Boolean {
+        val oldJob = jobs[p0]
+        val newJob = items[p1]
+
+        return oldJob.id == newJob.id
+    }
+
+    override fun getOldListSize(): Int = jobs.size
+
+    override fun getNewListSize(): Int = items.size
+
+    override fun areContentsTheSame(p0: Int, p1: Int): Boolean {
+        val oldJob = jobs[p0]
+        val newJob = items[p1]
+
+        return oldJob.id == newJob.id &&
+                oldJob.company == newJob.company &&
+                oldJob.description == newJob.description &&
+                oldJob.latitude == newJob.latitude &&
+                oldJob.location == newJob.location &&
+                oldJob.longitude == newJob.longitude &&
+                oldJob.salaryMax == newJob.salaryMax &&
+                oldJob.salaryMin == newJob.salaryMin &&
+                oldJob.title == newJob.title
+    }
+
+    /*override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        val oldJob = jobs[oldItemPosition]
+        val newJob = items[newItemPosition] as JobPresentation
+        val diffBundle = Bundle()
+
+        if (oldJob.title != newJob.title) diffBundle.putString(JobPresentationViewModel.TITLE, newJob.title)
+        if (oldJob.description != newJob.description) diffBundle.putString(JobPresentationViewModel.DESCRIPTION, newJob.description)
+        if (oldJob.company != newJob.company) diffBundle.putString(JobPresentationViewModel.COMPANY_TITLE, newJob.company?.displayName ?: "")
+        if (oldJob.latitude != newJob.latitude) diffBundle.putDouble(JobPresentationViewModel.LATITUDE, newJob.latitude ?: 0.0)
+        if (oldJob.longitude != newJob.longitude) diffBundle.putDouble(JobPresentationViewModel.LONGITUDE, newJob.longitude ?: 0.0)
+        if (oldJob.location != newJob.location) diffBundle.putString(JobPresentationViewModel.LOCATION, newJob.location?.displayName ?: "")
+        if (oldJob.salaryMin != newJob.salaryMin) diffBundle.putDouble(JobPresentationViewModel.SALARY_FROM, newJob.salaryMin ?: 0.0)
+        if (oldJob.salaryMax != newJob.salaryMax) diffBundle.putDouble(JobPresentationViewModel.SALARY_TO, newJob.salaryMax ?: 0.0)
+
+        if (diffBundle.size() == 0) return null
+
+        return diffBundle
+    }*/
+
+    override fun call(): DiffUtil.DiffResult = DiffUtil.calculateDiff(this)
+
+}
