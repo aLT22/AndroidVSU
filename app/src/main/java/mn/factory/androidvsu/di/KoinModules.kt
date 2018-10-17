f99cd841c20694c07ad8ff2c853db2e4083f70f6
Alexey Turkin <altsworks@gmail.com>
2018-10-17 12:27:55 +0300

    remove redundant code and dependencies

--- app/src/main/java/mn/factory/androidvsu/di/KoinModules.kt
+++ app/src/main/java/mn/factory/androidvsu/di/KoinModules.kt
@@ -2,7 +2,6 @@ package mn.factory.androidvsu.di
 
 import mn.factory.androidvsu.model.adzuna.mapper.JobSearchResultToJobPresentationMapper
 import mn.factory.androidvsu.model.adzuna.mapper.VersionToVersionPresentationMapper
-import mn.factory.androidvsu.ui.ViewModelFactory
 import mn.factory.androidvsu.ui.adapter.rv.JobsRecyclerAdapter
 import mn.factory.androidvsu.ui.main.MainActivityViewModel
 import mn.factory.androidvsu.ui.main.adzuna.jobs.list.JobListViewModel
@@ -28,7 +27,6 @@ val appModule = module {
 
 val viewModule = module {
     factory { JobsRecyclerAdapter(get()) }
-    single { ViewModelFactory(get(), get()) }
 
     //Activity VMs
     viewModel { MainActivityViewModel() }
