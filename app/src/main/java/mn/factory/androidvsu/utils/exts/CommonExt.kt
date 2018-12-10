81835cbd19891ce74d677feb49f5a9a14a22d5b5
Alexey Turkin <altsworks@gmail.com>
2018-12-10 19:52:50 +0300

    code refactoring

--- app/src/main/java/mn/factory/androidvsu/utils/exts/CommonExt.kt
+++ app/src/main/java/mn/factory/androidvsu/utils/exts/CommonExt.kt
@@ -2,6 +2,7 @@ package mn.factory.androidvsu.utils.exts
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 
 /**
  * Created by Turkin A. on 07/12/2018.
@@ -12,6 +13,9 @@ import android.content.Intent
  * */
 inline fun <reified T : Any> newIntent(context: Context): Intent =
         Intent(context, T::class.java)
+
+inline fun newBrowserIntent(url: String): Intent =
+        Intent(Intent.ACTION_VIEW, Uri.parse(url))
 /**
  * Intent extension block ends
  * */
