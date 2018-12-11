4257850c7e01cf756c9b0a5a1e6a2145d2954377
Alexey Turkin <altsworks@gmail.com>
2018-12-11 20:01:20 +0300

    refactoring

--- app/src/main/java/mn/factory/androidvsu/ui/main/adzuna/jobs/settings/JobSettingsVM.kt
+++
@@ -1,9 +0,0 @@
-package mn.factory.androidvsu.ui.main.adzuna.jobs.settings
-
-import androidx.lifecycle.ViewModel
-
-/**
- * Created by Turkin A. on 21/11/2018.
- */
-class JobSettingsVM : ViewModel() {
-}
